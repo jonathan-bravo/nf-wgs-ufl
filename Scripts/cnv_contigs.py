4296592c2e2d5df7e8672996ae0db60675e6c048
HaKmonkey <HaKmonkey@gmail.com>
2021-03-10 16:32:55 -0500

    Added documentation
    Added docstrings to the functions. Still need to add a docstring to
    `main()` and `contig()`. Will also probably change the format of the
    reports that are outputed. Will also include the sample name arg
    into the names of the reports.

--- Scripts/cnv_contigs.py
+++ Scripts/cnv_contigs.py
@@ -4,33 +4,68 @@ import argparse
 from os import path
 from pysam import VariantFile
 
-##  ~/Documents/nf-wgs-ufl/Scripts/contiguous.py -c NQ-21-01-BC708501-385_S25_filtered_cnv.vcf.gz
+def parse_args():
+    """
+    This function parses the input arguments.
 
-parser = argparse.ArgumentParser(description='Compare overlaps with a CNV vcf file and a standard SNV vcf file')
-parser.add_argument(
-    '-c',
-    metavar  = '--CNV',
-    type     = str,
-    help     = 'name of the CNV vcf',
-    required = True
-)
-parser.add_argument(
-    '-o',
-    metavar  = '--OUT',
-    type     = str,
-    help     = 'name of the outputfile'
-)
+    Keyword Arguments:
 
+    -c, --CNV:
+        the input CNV VCF filtered to just deletions or duplications
+    
+    -s, --SAMPLE_NAME:
+        the desired sample name to be included in the report name
 
+    Return:
 
-# This function will pull all entries from the vcf file into a list and
-# return the list
+    args -- the parsed arguments
+
+    """
+    parser = argparse.ArgumentParser(
+        description = 'inputs for generating multiple reports for CNV contigs'
+    )
+    parser.add_argument(
+        '-c',
+        metavar  = '--CNV',
+        type     = str,
+        help     = 'name of the CNV vcf',
+        required = True
+    )
+    parser.add_argument(
+        '-s',
+        metavar = '--SAMPLE_NAME',
+        type = str,
+        help = 'the sample name'
+    )
+    args = parser.parse_args()
+    return args
+
 def get_entries(vcf):
-    # a list that will contain a tuple for every contig from the filtered
-    # cnv vcf file
+    """
+    This function parses the input CNV file.
+
+    This functions takes a CNV variant call file that has been filtered to only
+    deletions and duplications, and stores each hit as an entry.
+
+    The structure of an entry is as follows for any given entry at index `i`:
+
+        * entries[i][0] = chrom
+        * entries[i][1] = start
+        * entries[i][2] = stop
+        * entries[i][3] = cyto
+        * entries[i][4] = alt
+        * entries[i][5] = genes
+
+    Keyword Arguments:
+
+    vcf -- the input VCF from `args.c`
+
+    Return:
+
+    entries -- a list of all the hits from the VCF
+
+    """
     entries = []
-    # a for loop that will append all the vcf entries from the filtered cnv vcf
-    # as a tuple into the contig list
     for entry in vcf.fetch():
         entries.append((
             entry.chrom,
@@ -40,35 +75,37 @@ def get_entries(vcf):
             entry.alts,
             entry.info["GENE"]
         ))
-    # Structure of entry:
-    # entries[i][0] = chrom
-    # entries[i][1] = start
-    # entries[i][2] = stop
-    # entries[i][3] = cyto
-    # entries[i][4] = alt
-    # entries[i][5] = genes
-    # Returning the list of entries from the vcf file
     return entries
-
-
 
 # This function will be for checking if the second entry is contiguous with
 # the first entry
 # ex: first = entries[0], second = entries[1]
 def check_overlap(first, second):
-    # check that the cytoband matched
+    """
+    This function checks if two CNVs overlap.
+
+    First we check if the called CNVs are both in the same cytoband. If this
+    is true we then check to see if the CNVs are of the same type, either
+    a deletion or duplication. Then we check to see if the starting position
+    of the second CNV is contained within the first CNV. If all the checks
+    are passed then we return a boolean value of `True` or `False` followed
+    by the genes from overlapping CNV.
+
+    Keyword Arguments:
+
+    first  -- the CNV at the end of the current contig
+    second -- the CNV that we are looking for an overlap in
+
+    Return:
+
+    success   -- a boolean value of either True or False
+    gene_list -- a list of genes from `second` or None if `success` is False
+
+    """
     if second[3] == first[3]:
-        # check that the alt matchs (DEL or DUP)
         if second[4] == first[4]:
-            # check that the second entry overlaps with the first
-            # second START is before the first END
-            # If everything passes we teturn a tuple that containes our answer
-            # and the genes from the second entry to append to our genes list
             if second[1]+1 < first[2]:
                 return (True, second[5])
-    # If the second entry is not contiguous return false. The return is a
-    # tuple of False, and None to maintain the correct formatting throughout
-    # the function 
     return (False, None)
             
 
@@ -131,39 +168,58 @@ def contig(start_index, entries):
 
 
 def generate_report(cnvs):
-    """Generates the final reports
+    """
+    Generates the final reports.
 
     This function takes the CNV contigs and prints them out into four different
     reports. There is a report for all duplications, all deletions, only
     duplications that contain genes, and only deletions that contain genes.
+
+    Keyword arguments:
+
+    cnvs -- the cnv contigs generated from `contig()`
+
+    Output:
+
+    duplications_report.tsv
+    duplications_genes_only_report.tsv
+    deletions_report.tsv
+    deletions_genes_only_report.tsv
+    
     """
-        
-    f = open("report.txt", "w")
+    a = open("duplications_report.tsv", "w")
+    b = open("duplications_genes_only_report.tsv", "w")
+    c = open("deletions_report.tsv", "w")
+    d = open("deletions_genes_only_report.tsv", "w")
+
     for i in range(len(cnvs)):
-        entry = '{chrom}\t{cyto}\t{cnv}\t{start}\t{stop}\t{genes}\n'.format(
-            chrom = cnvs[i][0],
-            cyto  = cnvs[i][1],
-            cnv   = cnvs[i][2]
-                .replace('\'', '')
-                .replace('(', '')
-                .replace(')', '')
-                .replace(',', ''),
-            start = cnvs[i][3],
-            stop  = cnvs[i][4],
-            genes = cnvs[i][5]
-                .replace('\"', '')
-                .replace('\'', '')
-                .replace('(', '')
-                .replace(')', '')
-                .replace('[', '')
-                .replace(']', '')
-        )
-        f.write(entry)
+        chrom = cnvs[i][0]
+        cyto  = cnvs[i][1]
+        cnv   = cnvs[i][2].replace('\'', '').replace('(', '').replace(')', '').replace(',', '')
+        start = cnvs[i][3]
+        stop  = cnvs[i][4]
+        genes = cnvs[i][5].replace('\"', '').replace('\'', '').replace('(', '').replace(')', '').replace('[', '').replace(']', '')
+        
+        entry = f'{chrom}\t{cyto}\t{cnv}\t{start}\t{stop}\t{genes}\n'
+        
+        if cnv == 'CNV: DUP':
+            a.write(entry)
+            if genes != 'GENES: ':
+                b.write(entry)
+        elif cnv == 'CNV: DEL':
+            c.write(entry)
+            if genes != 'GENES: ':
+                d.write(entry)
+    
+    a.close()
+    b.close()
+    c.close()
+    d.close()
 
 
 
 def main():
-    args = parser.parse_args()
+    args = parse_args()
     vcf = VariantFile(args.c)
 
     cnvs = []
