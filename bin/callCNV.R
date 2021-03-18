7bd78e3aefaa0b4f2895395cf2a2f5b7a6b42625
HaKmonkey <HaKmonkey@gmail.com>
2021-03-18 13:17:53 -0400

    made it so the sex of the sample is included in the CNV generation so we get sex chromosomes

--- bin/callCNV.R
+++ bin/callCNV.R
@@ -1,31 +1,45 @@
 #!/usr/bin/env Rscript
 
-args = commandArgs(trailingOnly=TRUE)
+args = commandArgs(trailingOnly = TRUE)
 sample_id = args[1]
-control_data = args[2]
+male_control_data = args[2]
+female_control_data = args[3]
 
 library(panelcn.mops)
 
-load(file=control_data)
+m_or_f <- paste(sample_id, "_m_or_f.txt", sep = '')
+sample_sex <- read.table(m_or_f, header = FALSE)
 
-testBam <- paste(sample_id,"-sort.bam",sep='')
+if(sample_sex[1, 1] == "Female"){
+    sex <- "Female"
+    load(file = female_control_data)
+} else {
+    sex <- "Male"
+    load(file = male_control_data)
+}
+
+testBam <- paste(sample_id, "-sort.bam", sep = '')
 
 test <- countBamListInGRanges(
-countWindows = countWindows,
-bam.files    = testBam,
-read.width   = 150)
+    countWindows = countWindows,
+    bam.files    = testBam,
+    read.width   = 150
+)
 
 XandCB <- test
 elementMetadata(XandCB) <- cbind(
     elementMetadata(XandCB),
-    elementMetadata(control))
+    elementMetadata(control)
+)
 
 resultsList <- runPanelcnMops(
     XandCB,
     testiv        = 1:ncol(elementMetadata(test)),
     countWindows  = countWindows,
     selectedGenes = NULL,
-    maxControls   = 25)
+    maxControls   = 25,
+    sex           = sex
+)
 
 sampleNames <- colnames(elementMetadata(test))
 resulttable <- createResultTable(
@@ -33,6 +47,11 @@ resulttable <- createResultTable(
     XandCB        = XandCB,
     countWindows  = countWindows,
     selectedGenes = NULL,
-    sampleNames   = sampleNames)
+    sampleNames   = sampleNames
+)
 
-write.table(resulttable[[1]], paste(sample_id,'_cnv_table.csv',sep=''), append = TRUE)
+write.table(
+    resulttable[[1]],
+    paste(sample_id, '_cnv_table.csv', sep = ''),
+    append = TRUE
+)
