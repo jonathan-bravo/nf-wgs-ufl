2c3314691bf67fc824390ed5a334562814424988
HaKmonkey <HaKmonkey@gmail.com>
2021-05-04 18:23:56 -0400

    a script to make connecting to my instance easier

---
+++ Scripts/bs-to-aws-instance-connect.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+DNS=$(
+    dns_list=$(
+        aws ec2 describe-instances --instance-ids i-0a21f1098dcb1575a --query 'Reservations[].Instances[].PublicDnsName'
+    )
+    echo ${dns_list} | 
+    cut -c4-$(expr ${#dns_list} - 7)
+);
+STATUS=$(
+    status_list=$(
+        aws ec2 describe-instances --instance-ids i-0a21f1098dcb1575a --query  'Reservations[].Instances[].State.Name'
+    )
+    echo ${status_list} | 
+    cut -c4-$(expr ${#status_list} - 7)
+);
+
+wait_running () {
+    while [[ ${STATUS} != "running" ]];
+    do
+        echo "Checking if status is running...";
+        STATUS=$(
+            status_list=$(
+                aws ec2 describe-instances --instance-ids i-0a21f1098dcb1575a --query  'Reservations[].Instances[].State.Name'
+            )
+            echo ${status_list} | 
+            cut -c4-$(expr ${#status_list} - 7)
+        );
+        echo "Status is "${STATUS}"...";
+        sleep 10s;
+    done
+}
+
+wait_stopped () {
+    while [[ ${STATUS} != "stopped" ]];
+    do
+        echo "Checking if status is stopped...";
+        STATUS=$(
+            status_list=$(
+                aws ec2 describe-instances --instance-ids i-0a21f1098dcb1575a --query  'Reservations[].Instances[].State.Name'
+            )
+            echo ${status_list} | 
+            cut -c4-$(expr ${#status_list} - 7)
+        );
+        echo "Status is "${STATUS}"...";
+        sleep 10s;
+    done
+}
+
+if [[ ${STATUS} == "stopped" ]];
+then 
+    aws ec2 start-instances --instance-ids i-0a21f1098dcb1575a;
+    echo "Starting...";
+    wait_running;
+    ssh -i "~/Documents/j.bravo.pem" ubuntu@${DNC};
+elif [[ ${STATUS} == "stopping" ]];
+then
+    echo "Waiting for instance to be ready to start...";
+    wait_stopped;
+    aws ec2 start-instances --instance-ids i-0a21f1098dcb1575a;
+    echo "Starting...";
+    wait_running;
+    ssh -i "~/Documents/j.bravo.pem" ubuntu@${DNC};
+else
+    echo "Already on";
+    ssh -i "~/Documents/j.bravo.pem" ubuntu@${DNC};
+fi
