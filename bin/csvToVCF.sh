c5d230558578facad7a9cb59270c055278018987
HaKmonkey <HaKmonkey@gmail.com>
2021-05-10 21:21:51 -0400

    Fixed script so all genes get listed in VCF

--- bin/csvToVCF.sh
+++ bin/csvToVCF.sh
@@ -16,16 +16,19 @@ sed -i "s|##fileDate=|##fileDate=$TODAY|" ${sample_id}.head
 #sed -i "s|SAMPLE1|${sample_id}-sort|g" ${sample_id}.head
 sed -i '$ s/\s/\t/g' ${sample_id}.head
 
-cut -d "," -f 2,3,4,5,8,9,10,11 ${sample_id}_cnvs.csv | \
+cut -d "," -f 2,3,4,5,8,9,10,11- ${sample_id}_cnvs.csv | \
 awk -F',' 'NR>1 {
     printf $1"\t"$2"\tcn.MOPS:"$1":"$2"-"$3"\tN\t";
     if ($7 ~ "CN0" || $7 ~ "CN1") printf "DEL\t";
     else if ($7 ~ "CN3" || $7 ~ "CN4" || $7 ~ "CN5" || $7 ~ "CN6" || $7 ~ "CN7" || $7 ~ "CN8") printf "DUP\t";
     else printf ".\t";
     printf ".\tPASS\t";
-    printf "SVTYPE=CNV;END="$3";LENGTH="$4";CNCLASS="$7";GENES="$8"\tMED:MEAN\t"$5":"$6"\n";
+    printf "SVTYPE=CNV;END="$3";LENGTH="$4";CNCLASS="$7";GENES=";
+    for(i = 8; i <= NF; i++){printf $i","};
+    printf "\tMED:MEAN\t"$5":"$6"\n";
 }' >> ${sample_id}.tmp
 
+sed -i 's/",//g' ${sample_id}.tmp
 sed -i 's/"//g' ${sample_id}.tmp
 sed -i 's/GENES=\t/GENES=.\t/g' ${sample_id}.tmp
 
