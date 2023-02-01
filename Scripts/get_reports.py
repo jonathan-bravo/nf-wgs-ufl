afdda292b2f551b50b019a3faa290a5505fbd98b
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-01 15:22:50 -0500

    Update get_reports.py
    Now this script will archive the output

--- Scripts/get_reports.py
+++ Scripts/get_reports.py
@@ -2,7 +2,7 @@
 
 import boto3
 import argparse
-from os import mkdir
+from os import makedirs
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -24,21 +24,45 @@ def get_s3_resource():
     return s3
 
 def get_reports(bucket, s3, run_id):
-    mkdir(f'{run_id}_reports/')
+    makedirs(f'{run_id}/Reports/')
+    makedirs(f'{run_id}/MultiQC/multiqc_report_data/')
+
     my_bucket = s3.Bucket(bucket)
+
     data_source = my_bucket.objects.filter(Prefix = f'Pipeline_Output/{run_id}')
+    
     for objects in data_source:
+        copy_source = {
+            'Bucket': my_bucket.name,
+            'Key': objects.key
+        }
+        if objects.key.split('/')[2] == 'MultiQC':
+            filename = objects.key.split('/')[-1]
+            if '.html' in filename:
+                my_bucket.download_file(objects.key, f"{run_id}/MultiQC/{filename}")
+            else:
+                my_bucket.download_file(objects.key, f"{run_id}/MultiQC/multiqc_report_data/{filename}")
         report_file = (
             '_report.xlsx' in objects.key
             and 'low-qc_report.xlsx' not in objects.key
         )
         if report_file:
-            my_bucket.download_file(objects.key, f"{run_id}_reports/{objects.key.split('/')[-1]}")
+            my_bucket.download_file(objects.key, f"{run_id}/Reports/{objects.key.split('/')[-1]}")
+        s3.meta.client.copy(
+            copy_source,
+            my_bucket.name,
+            f'Pipeline_Output/_GLACIER_ARCHIVE/{run_id}/{"/".join(objects.key.split("/")[2:])}',
+            ExtraArgs = {
+                'StorageClass': 'DEEP_ARCHIVE',
+                'MetadataDirective': 'COPY'
+            }
+        )
+        #objects.delete()
 
 def main():
     args = parse_args()
     s3 = get_s3_resource()
-    get_samples('hakmonkey-genetics-lab', s3, args.r)
+    get_reports('hakmonkey-genetics-lab', s3, args.r)
 
 if __name__ == '__main__':
 	main()
