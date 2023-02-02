f7dd0f2eae01165b887aafb72939e0ba2fc8f7ed
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-01 22:42:07 -0500

    Update get_reports.py

--- Scripts/get_reports.py
+++ Scripts/get_reports.py
@@ -57,7 +57,7 @@ def get_reports(bucket, s3, run_id):
                 'MetadataDirective': 'COPY'
             }
         )
-        #objects.delete()
+        objects.delete()
 
 def main():
     args = parse_args()
