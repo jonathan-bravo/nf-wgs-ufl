d5e820d07fa89658c5fd7f684f11c15742547954
Johnny Bravo <HaKmonkey@gmail.com>
2023-02-10 16:54:22 -0500

    Update to_cgap_bucket.py
    Added meta_data excel sheet generation to this so that you don't have to do all the manual work of copying all the files name. Just have to adjust som of the default values.

--- Scripts/to_cgap_bucket.py
+++ Scripts/to_cgap_bucket.py
@@ -3,8 +3,27 @@
 import argparse
 import boto3
 import json
+import pandas as pd
 from itertools import chain
+from datetime import date
+from dataclasses import dataclass
 from concurrent.futures import ProcessPoolExecutor as ppe
+
+@dataclass
+class SampleEntry:
+    analysis_id:str = ''
+    unique_analysis_id:str = ''
+    individual_id:str = ''
+    sex:str = 'M/F'
+    birth_year:int = 1900
+    relation_to_proband:str = 'Proband'
+    specimen_type:str = 'Whole Blood'
+    specimen_id:str = ''
+    report_required:str = 'Yes'
+    test_requested:str = 'WGS'
+    sequencing_date:date = date.today().year
+    tags:str = 'MSA/ MSA_control'
+    files:str = ''
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -27,6 +46,21 @@ def grab_file_keys(data, source):
     key = lambda f: f.key
     return list(map(key, filter(trm, filter(sam, objects))))
 
+def gen_row(sample, files):
+    f_read = files[0].split('/')[-1].replace('-p_trimmed', '')
+    r_read = files[1].split('/')[-1].replace('-p_trimmed', '')
+    return SampleEntry(
+        analysis_id = sample,
+        unique_analysis_id = f_read.replace('_R1.fastq.gz', ''),
+        individual_id = sample,
+        specimen_id = sample,
+        files = f'{f_read}, {r_read}'
+    )
+
+def gen_xlsx(rows, outfile):
+    with pd.ExcelWriter(outfile, mode='w') as writer:
+        rows.to_excel(writer, index=False)
+
 def copy_file(f, source, dest):
     s3 = get_s3_resource()
     dest_bucket = s3.Bucket(dest)
@@ -41,13 +75,38 @@ def main():
     with open(args.json) as f:
         data = json.load(f)
 
+    samples = list(chain(*[data[run] for run in data]))
+    outfile = f'cgap-msa_accessioning_{len(samples)}-samples_{date.today()}.xlsx'
+
     source_list = [args.source]*len(data)
 
     with ppe(args.threads) as p:
-        files = chain(*list(p.map(grab_file_keys, data.items(), source_list)))
+        files = list(chain(*list(p.map(grab_file_keys, data.items(), source_list))))
 
-    source_list = [args.source]*sum(len(data[i]) for i in data)
-    dest_list = [args.dest]*sum(len(data[i]) for i in data)
+    file_pairs = [(files[i], files[i+1]) for i in range(0, len(files), 2)]
+
+    rows = pd.DataFrame(list(map(gen_row, samples, file_pairs)))
+    rows = rows[1:]
+    rows.columns = [
+        'Analysis ID*',
+        'Unique Analysis ID*',
+        'Individual ID*',
+        'Sex*',
+        'Birth Year',
+        'Relation to Proband*',
+        'Specimen Type*',
+        'Specimen ID*',
+        'Report Required*',
+        'Test Requested*',
+        'Sequencing Date',
+        'Tags',
+        'Files'
+    ]
+
+    gen_xlsx(rows, outfile)
+
+    source_list = [args.source]*len(samples)
+    dest_list = [args.dest]*len(samples)
 
     with ppe(args.threads) as p:
         p.map(copy_file, files, source_list, dest_list)
